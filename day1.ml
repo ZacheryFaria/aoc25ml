74b1a5422bf8fd3fe44ba36be8124a1dfcb6eb50
Zach Faria <zfaria@anduril.com>
2025-12-22 12:22:40 -0800

    solve day 1

--- day1.ml
+++ day1.ml
@@ -1,32 +1,43 @@
-
-let readfile filename = In_channel.with_open_text filename In_channel.input_lines
+let readfile filename =
+  In_channel.with_open_text filename In_channel.input_lines
 
 type direction = Right | Left
+type instruction = { dir : direction; value : int }
 
-type instruction = {dir: direction; value: int}
+(* let char_of_instruction ins = match ins with Right -> 'R' | Left -> 'L'
 
+let string_of_instruction ins =
+  Format.sprintf "Instruction{dir: %c; value: %d}"
+    (char_of_instruction ins.dir)
+    ins.value
 
-let char_of_instruction ins = match ins with
-| Right -> 'R'
-| Left -> 'L'
+let print_instruction ins = print_endline (string_of_instruction ins) *)
 
-let string_of_instruction ins = Format.sprintf "Instruction{dir: %c; value: %d}" (char_of_instruction ins.dir) ins.value
+let direction_from_row row =
+  match String.get row 0 with 'R' -> Right | 'L' -> Left | _ -> assert false
 
-let direction_from_row row = match (String.get row 0) with
-| 'R' -> Right
-| 'L' -> Left
-| _ -> assert false
+let number_from_row row =
+  String.sub row 1 (String.length row - 1) |> int_of_string
 
-let number_from_row row = 
-    String.sub row 1 ((String.length row) - 1)
-    |> int_of_string
+let parse_row value =
+  { dir = direction_from_row value; value = number_from_row value }
 
-let parse_row value = {dir = (direction_from_row value); value = (number_from_row value)}
+let apply_instruction curr ins =
+  match ins.dir with Right -> curr + ins.value | Left -> curr - ins.value
 
-let print_instruction ins = 
-    print_endline (string_of_instruction ins)
+let normalize value =
+  let mod_value = value mod 100 in
+  if mod_value >= 0 then mod_value else 100 + mod_value
 
-let _ = 
-    let mapper = List.map parse_row in
-    let printer = List.map print_instruction in
-    readfile "day1.txt" |> mapper |> printer 
+let rec solve instructions curr count =
+  match instructions with
+  | [] -> count
+  | hd :: tl ->
+      let result = apply_instruction curr hd |> normalize in
+      let _ = print_int result in
+      let _ = print_endline "" in
+      solve tl result (count + if result == 0 then 1 else 0)
+
+let _ =
+  let input = List.map parse_row (readfile "day1.txt") in
+  solve input 50 0 |> print_int
